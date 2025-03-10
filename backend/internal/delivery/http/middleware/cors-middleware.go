aca565a902bbeb3edb5e2f6d4ae0df5b0bddc7f9
Mogilin_Nikita <mns22u947@student.bmstu.ru>
2025-03-10 12:51:37 +0300

    #4

--- backend/internal/delivery/http/middleware/cors-middleware.go
+++ backend/internal/delivery/http/middleware/cors-middleware.go
@@ -12,20 +12,13 @@ func CORSMiddleware(config config.CORSConfig) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			origin := r.Header.Get("Origin")
 
-			// Разрешенные домены
-			allowedOrigins := map[string]bool{
-				"https://www.quickflowapp.ru": true,
-			}
-
 			// Проверяем, не установлен ли уже CORS-заголовок
 			if _, exists := w.Header()["Access-Control-Allow-Origin"]; !exists {
-				if allowedOrigins[origin] {
-					w.Header().Set("Access-Control-Allow-Origin", origin)
-					w.Header().Set("Vary", "Origin")
-					w.Header().Set("Access-Control-Allow-Credentials", "true")
-					w.Header().Set("Access-Control-Allow-Methods", strings.Join(config.AllowedMethods, ", "))
-					w.Header().Set("Access-Control-Allow-Headers", strings.Join(config.AllowedHeaders, ", "))
-				}
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				w.Header().Set("Vary", "Origin")
+				w.Header().Set("Access-Control-Allow-Credentials", "true")
+				w.Header().Set("Access-Control-Allow-Methods", strings.Join(config.AllowedMethods, ", "))
+				w.Header().Set("Access-Control-Allow-Headers", strings.Join(config.AllowedHeaders, ", "))
 			}
 
 			// Если OPTIONS-запрос, отвечаем 204
