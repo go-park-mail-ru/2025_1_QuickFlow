2550fc85a0e358eb365262a9319932821ac32168
Nok1o <nikitamogilin39@gmail.com>
2025-04-15 18:51:56 +0300

    username for chats.

--- backend/internal/delivery/forms/chat-forms.go
+++ backend/internal/delivery/forms/chat-forms.go
@@ -27,6 +27,12 @@ type ChatOut struct {
 	LastMessage *MessageOut `json:"last_message,omitempty"`
 	IsOnline    *bool       `json:"online,omitempty"`
 	LastSeen    string      `json:"last_seen,omitempty"`
+	Username    string      `json:"username,omitempty"`
+}
+
+type PrivateChatInfo struct {
+	Username string   `json:"username,omitempty"`
+	Activity Activity `json:"activity,omitempty"`
 }
 
 func (g *GetChatsForm) GetParams(values url.Values) error {
@@ -54,7 +60,7 @@ func (g *GetChatsForm) GetParams(values url.Values) error {
 	return nil
 }
 
-func ToChatsOut(chats []models.Chat, lastMessageSenderInfo map[uuid.UUID]models.PublicUserInfo, privateChatsOnlineStatus map[uuid.UUID]Activity) []ChatOut {
+func ToChatsOut(chats []models.Chat, lastMessageSenderInfo map[uuid.UUID]models.PublicUserInfo, privateChatsOnlineStatus map[uuid.UUID]PrivateChatInfo) []ChatOut {
 	var chatsOut []ChatOut
 	var chatType string
 	for _, chat := range chats {
@@ -80,12 +86,12 @@ func ToChatsOut(chats []models.Chat, lastMessageSenderInfo map[uuid.UUID]models.
 		}
 
 		if chat.Type == models.ChatTypePrivate && privateChatsOnlineStatus != nil {
-			if activity, exists := privateChatsOnlineStatus[chat.ID]; exists {
-				chatOut.IsOnline = &activity.IsOnline
-				if !activity.IsOnline {
-					chatOut.LastSeen = activity.LastSeen
+			if profileInfo, exists := privateChatsOnlineStatus[chat.ID]; exists {
+				chatOut.IsOnline = &profileInfo.Activity.IsOnline
+				if !profileInfo.Activity.IsOnline {
+					chatOut.LastSeen = profileInfo.Activity.LastSeen
 				}
-
+				chatOut.Username = profileInfo.Username
 			}
 		}
 		chatsOut = append(chatsOut, chatOut)
--- backend/internal/delivery/http/post-handler.go
+++ backend/internal/delivery/http/post-handler.go
@@ -1,33 +1,33 @@
 package http
 
 import (
-    "encoding/json"
-    "errors"
-    "fmt"
-    "net/http"
-    "strconv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
 
-    "github.com/google/uuid"
-    "github.com/gorilla/mux"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 
-    "quickflow/internal/delivery/forms"
-    "quickflow/internal/models"
-    "quickflow/internal/usecase"
-    "quickflow/pkg/logger"
-    http2 "quickflow/utils/http"
+	"quickflow/internal/delivery/forms"
+	"quickflow/internal/models"
+	"quickflow/internal/usecase"
+	"quickflow/pkg/logger"
+	http2 "quickflow/utils/http"
 )
 
 type PostHandler struct {
-    postUseCase    PostUseCase
-    profileUseCase ProfileUseCase
+	postUseCase    PostUseCase
+	profileUseCase ProfileUseCase
 }
 
 // NewPostHandler creates new post handler.
 func NewPostHandler(postUseCase PostUseCase, profileUseCase ProfileUseCase) *PostHandler {
-    return &PostHandler{
-        postUseCase:    postUseCase,
-        profileUseCase: profileUseCase,
-    }
+	return &PostHandler{
+		postUseCase:    postUseCase,
+		profileUseCase: profileUseCase,
+	}
 }
 
 // AddPost добавляет новый пост
@@ -43,79 +43,79 @@ func NewPostHandler(postUseCase PostUseCase, profileUseCase ProfileUseCase) *Pos
 // @Failure 500 {object} forms.ErrorForm "Ошибка сервера"
 // @Router /api/post [post]
 func (p *PostHandler) AddPost(w http.ResponseWriter, r *http.Request) {
-    // extracting user from context
-    ctx := r.Context()
-    user, ok := ctx.Value("user").(models.User)
-    if !ok {
-        logger.Error(ctx, "Failed to get user from context while adding post")
-        http2.WriteJSONError(w, "Failed to get user from context", http.StatusInternalServerError)
-        return
-    }
-    logger.Info(ctx, fmt.Sprintf("User %s requested to add post", user.Username))
+	// extracting user from context
+	ctx := r.Context()
+	user, ok := ctx.Value("user").(models.User)
+	if !ok {
+		logger.Error(ctx, "Failed to get user from context while adding post")
+		http2.WriteJSONError(w, "Failed to get user from context", http.StatusInternalServerError)
+		return
+	}
+	logger.Info(ctx, fmt.Sprintf("User %s requested to add post", user.Username))
 
-    err := r.ParseMultipartForm(10 << 20) // 10 MB
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to parse form: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to parse form", http.StatusBadRequest)
-        return
-    }
+	err := r.ParseMultipartForm(10 << 20) // 10 MB
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to parse form: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to parse form", http.StatusBadRequest)
+		return
+	}
 
-    // parsing JSON
-    var postForm forms.PostForm
-    postForm.Text = r.FormValue("text")
-    isRepostString := r.FormValue("is_repost")
+	// parsing JSON
+	var postForm forms.PostForm
+	postForm.Text = r.FormValue("text")
+	isRepostString := r.FormValue("is_repost")
 
-    if len(postForm.Text) > 1000 {
-        logger.Error(ctx, fmt.Sprintf("Text length validation failed: length=%d", len(postForm.Text)))
-        http2.WriteJSONError(w, "Text must be between 1 and 1000 characters", http.StatusBadRequest)
-        return
+	if len(postForm.Text) > 1000 {
+		logger.Error(ctx, fmt.Sprintf("Text length validation failed: length=%d", len(postForm.Text)))
+		http2.WriteJSONError(w, "Text must be between 1 and 1000 characters", http.StatusBadRequest)
+		return
 
-    }
+	}
 
-    if len(isRepostString) != 0 {
-        postForm.IsRepost, err = strconv.ParseBool(r.FormValue("is_repost"))
-        if err != nil {
-            logger.Error(ctx, fmt.Sprintf("Failed to parse is_repost: %s", err.Error()))
-            http2.WriteJSONError(w, "Failed to parse form", http.StatusBadRequest)
-            return
-        }
-    }
+	if len(isRepostString) != 0 {
+		postForm.IsRepost, err = strconv.ParseBool(r.FormValue("is_repost"))
+		if err != nil {
+			logger.Error(ctx, fmt.Sprintf("Failed to parse is_repost: %s", err.Error()))
+			http2.WriteJSONError(w, "Failed to parse form", http.StatusBadRequest)
+			return
+		}
+	}
 
-    postForm.Images, err = http2.GetFiles(r, "pics")
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to get files: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to get files", http.StatusBadRequest)
-        return
-    }
-    logger.Info(ctx, fmt.Sprintf("Recieved post: %+v", postForm))
+	postForm.Images, err = http2.GetFiles(r, "pics")
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to get files: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to get files", http.StatusBadRequest)
+		return
+	}
+	logger.Info(ctx, fmt.Sprintf("Recieved post: %+v", postForm))
 
-    post := postForm.ToPostModel(user.Id)
+	post := postForm.ToPostModel(user.Id)
 
-    post, err = p.postUseCase.AddPost(ctx, post)
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to add post: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to add post", http.StatusInternalServerError)
-        return
-    }
-    logger.Info(ctx, "Successfully added post")
+	post, err = p.postUseCase.AddPost(ctx, post)
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to add post: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to add post", http.StatusInternalServerError)
+		return
+	}
+	logger.Info(ctx, "Successfully added post")
 
-    var postOut forms.PostOut
-    postOut.FromPost(post)
-    publicUserInfo, err := p.profileUseCase.GetPublicUserInfo(ctx, user.Id)
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to get public user info: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to get public user info", http.StatusInternalServerError)
-        return
-    }
-    postOut.Creator = forms.PublicUserInfoToOut(publicUserInfo, models.RelationSelf)
+	var postOut forms.PostOut
+	postOut.FromPost(post)
+	publicUserInfo, err := p.profileUseCase.GetPublicUserInfo(ctx, user.Id)
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to get public user info: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to get public user info", http.StatusInternalServerError)
+		return
+	}
+	postOut.Creator = forms.PublicUserInfoToOut(publicUserInfo, models.RelationSelf)
 
-    w.Header().Set("Content-Type", "application/json")
-    err = json.NewEncoder(w).Encode(forms.PayloadWrapper[forms.PostOut]{Payload: postOut})
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to encode post: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to encode post", http.StatusInternalServerError)
-        return
-    }
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(forms.PayloadWrapper[forms.PostOut]{Payload: postOut})
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to encode post: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to encode post", http.StatusInternalServerError)
+		return
+	}
 }
 
 // DeletePost удаляет пост
@@ -130,45 +130,45 @@ func (p *PostHandler) AddPost(w http.ResponseWriter, r *http.Request) {
 // @Failure 500 {object} forms.ErrorForm "Ошибка сервера"
 // @Router /api/posts/{post_id} [delete]
 func (p *PostHandler) DeletePost(w http.ResponseWriter, r *http.Request) {
-    // extracting user from context
-    ctx := r.Context()
-    user, ok := ctx.Value("user").(models.User)
-    if !ok {
-        logger.Error(ctx, "Failed to get user from context while deleting post")
-        http2.WriteJSONError(w, "Failed to get user from context", http.StatusInternalServerError)
-        return
-    }
+	// extracting user from context
+	ctx := r.Context()
+	user, ok := ctx.Value("user").(models.User)
+	if !ok {
+		logger.Error(ctx, "Failed to get user from context while deleting post")
+		http2.WriteJSONError(w, "Failed to get user from context", http.StatusInternalServerError)
+		return
+	}
 
-    postIdString := mux.Vars(r)["post_id"]
-    if len(postIdString) == 0 {
-        logger.Error(ctx, "Failed to get post id from request")
-        http2.WriteJSONError(w, "Failed to get post id", http.StatusBadRequest)
-        return
-    }
-    logger.Info(ctx, fmt.Sprintf("User %s requested to delete post with id %s", user.Username, postIdString))
+	postIdString := mux.Vars(r)["post_id"]
+	if len(postIdString) == 0 {
+		logger.Error(ctx, "Failed to get post id from request")
+		http2.WriteJSONError(w, "Failed to get post id", http.StatusBadRequest)
+		return
+	}
+	logger.Info(ctx, fmt.Sprintf("User %s requested to delete post with id %s", user.Username, postIdString))
 
-    postId, err := uuid.Parse(postIdString)
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to parse post id: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to parse post id", http.StatusBadRequest)
-        return
-    }
+	postId, err := uuid.Parse(postIdString)
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to parse post id: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to parse post id", http.StatusBadRequest)
+		return
+	}
 
-    err = p.postUseCase.DeletePost(ctx, user, postId)
-    if errors.Is(err, usecase.ErrPostDoesNotBelongToUser) {
-        logger.Error(ctx, fmt.Sprintf("Post %s does not belong to user %s", postIdString, user.Username))
-        http2.WriteJSONError(w, "Post does not belong to user", http.StatusForbidden)
-        return
-    } else if errors.Is(err, usecase.ErrPostNotFound) {
-        logger.Error(ctx, fmt.Sprintf("Post %s not found", postIdString))
-        http2.WriteJSONError(w, "Post not found", http.StatusNotFound)
-        return
-    } else if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to delete post: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed delete post", http.StatusInternalServerError)
-        return
-    }
-    logger.Info(ctx, fmt.Sprintf("Successfully deleted post %s", postIdString))
+	err = p.postUseCase.DeletePost(ctx, user, postId)
+	if errors.Is(err, usecase.ErrPostDoesNotBelongToUser) {
+		logger.Error(ctx, fmt.Sprintf("Post %s does not belong to user %s", postIdString, user.Username))
+		http2.WriteJSONError(w, "Post does not belong to user", http.StatusForbidden)
+		return
+	} else if errors.Is(err, usecase.ErrPostNotFound) {
+		logger.Error(ctx, fmt.Sprintf("Post %s not found", postIdString))
+		http2.WriteJSONError(w, "Post not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to delete post: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed delete post", http.StatusInternalServerError)
+		return
+	}
+	logger.Info(ctx, fmt.Sprintf("Successfully deleted post %s", postIdString))
 }
 
 // UpdatePost updates a post
@@ -186,75 +186,75 @@ func (p *PostHandler) DeletePost(w http.ResponseWriter, r *http.Request) {
 // @Failure 500 {object} forms.ErrorForm "Server error"
 // @Router /api/post [put]
 func (p *PostHandler) UpdatePost(w http.ResponseWriter, r *http.Request) {
-    // extracting user from context
-    ctx := r.Context()
-    user, ok := ctx.Value("user").(models.User)
-    if !ok {
-        logger.Error(ctx, "Failed to get user from context while updating post")
-        http2.WriteJSONError(w, "Failed to get user from context", http.StatusInternalServerError)
-        return
-    }
+	// extracting user from context
+	ctx := r.Context()
+	user, ok := ctx.Value("user").(models.User)
+	if !ok {
+		logger.Error(ctx, "Failed to get user from context while updating post")
+		http2.WriteJSONError(w, "Failed to get user from context", http.StatusInternalServerError)
+		return
+	}
 
-    postIdString := mux.Vars(r)["post_id"]
-    postId, err := uuid.Parse(postIdString)
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to parse post id: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to parse post id", http.StatusBadRequest)
-        return
-    }
+	postIdString := mux.Vars(r)["post_id"]
+	postId, err := uuid.Parse(postIdString)
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to parse post id: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to parse post id", http.StatusBadRequest)
+		return
+	}
 
-    err = r.ParseMultipartForm(10 << 20) // 10 MB
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to parse form: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to parse form", http.StatusBadRequest)
-        return
-    }
+	err = r.ParseMultipartForm(10 << 20) // 10 MB
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to parse form: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to parse form", http.StatusBadRequest)
+		return
+	}
 
-    var updatePostForm forms.UpdatePostForm
-    updatePostForm.Text = r.FormValue("text")
-    updatePostForm.Images, err = http2.GetFiles(r, "pics")
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to get files: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to get files", http.StatusBadRequest)
-        return
-    }
+	var updatePostForm forms.UpdatePostForm
+	updatePostForm.Text = r.FormValue("text")
+	updatePostForm.Images, err = http2.GetFiles(r, "pics")
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to get files: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to get files", http.StatusBadRequest)
+		return
+	}
 
-    updatePost, err := updatePostForm.ToPostUpdateModel(postId)
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to parse update post: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to parse update post", http.StatusBadRequest)
-        return
-    }
-    post, err := p.postUseCase.UpdatePost(ctx, updatePost, user.Id)
-    if errors.Is(err, usecase.ErrPostDoesNotBelongToUser) {
-        logger.Error(ctx, fmt.Sprintf("Post %s does not belong to user %s", postIdString, user.Username))
-        http2.WriteJSONError(w, "Post does not belong to user", http.StatusForbidden)
-        return
-    } else if errors.Is(err, usecase.ErrPostNotFound) {
-        logger.Error(ctx, fmt.Sprintf("Post %s not found", postIdString))
-        http2.WriteJSONError(w, "Post not found", http.StatusNotFound)
-        return
-    } else if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to update post: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to update post", http.StatusInternalServerError)
-        return
-    }
+	updatePost, err := updatePostForm.ToPostUpdateModel(postId)
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to parse update post: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to parse update post", http.StatusBadRequest)
+		return
+	}
+	post, err := p.postUseCase.UpdatePost(ctx, updatePost, user.Id)
+	if errors.Is(err, usecase.ErrPostDoesNotBelongToUser) {
+		logger.Error(ctx, fmt.Sprintf("Post %s does not belong to user %s", postIdString, user.Username))
+		http2.WriteJSONError(w, "Post does not belong to user", http.StatusForbidden)
+		return
+	} else if errors.Is(err, usecase.ErrPostNotFound) {
+		logger.Error(ctx, fmt.Sprintf("Post %s not found", postIdString))
+		http2.WriteJSONError(w, "Post not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to update post: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to update post", http.StatusInternalServerError)
+		return
+	}
 
-    logger.Info(ctx, fmt.Sprintf("Successfully updated post %s", postIdString))
-    var postOut forms.PostOut
-    postOut.FromPost(post)
-    publicUserInfo, err := p.profileUseCase.GetPublicUserInfo(ctx, user.Id)
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to get public user info: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to get public user info", http.StatusInternalServerError)
-        return
-    }
-    postOut.Creator = forms.PublicUserInfoToOut(publicUserInfo, models.RelationSelf)
-    w.Header().Set("Content-Type", "application/json")
-    err = json.NewEncoder(w).Encode(forms.PayloadWrapper[forms.PostOut]{Payload: postOut})
-    if err != nil {
-        logger.Error(ctx, fmt.Sprintf("Failed to encode post: %s", err.Error()))
-        http2.WriteJSONError(w, "Failed to encode post", http.StatusInternalServerError)
-        return
-    }
+	logger.Info(ctx, fmt.Sprintf("Successfully updated post %s", postIdString))
+	var postOut forms.PostOut
+	postOut.FromPost(post)
+	publicUserInfo, err := p.profileUseCase.GetPublicUserInfo(ctx, user.Id)
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to get public user info: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to get public user info", http.StatusInternalServerError)
+		return
+	}
+	postOut.Creator = forms.PublicUserInfoToOut(publicUserInfo, models.RelationSelf)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(forms.PayloadWrapper[forms.PostOut]{Payload: postOut})
+	if err != nil {
+		logger.Error(ctx, fmt.Sprintf("Failed to encode post: %s", err.Error()))
+		http2.WriteJSONError(w, "Failed to encode post", http.StatusInternalServerError)
+		return
+	}
 }
