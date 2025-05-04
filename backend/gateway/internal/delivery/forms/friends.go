cca03cf60f45d4fc8867ca22d3fbf0c65f83ffe7
Nok1o <nikitamogilin39@gmail.com>
2025-05-04 23:12:22 +0300

    payload.

--- backend/gateway/internal/delivery/forms/friends.go
+++ backend/gateway/internal/delivery/forms/friends.go
@@ -1,53 +1,53 @@
 package forms
 
 import (
-	"github.com/google/uuid"
+    "github.com/google/uuid"
 
-	"quickflow/shared/models"
+    "quickflow/shared/models"
 )
 
 type FriendRequest struct {
-	ReceiverID string `json:"receiver_id"`
+    ReceiverID string `json:"receiver_id"`
 }
 
 type FriendRequestDel struct {
-	FriendID string `json:"friend_id"`
+    FriendID string `json:"friend_id"`
 }
 
 type FriendsInfoOut struct {
-	ID         uuid.UUID `json:"id"`
-	Username   string    `json:"username"`
-	FirstName  string    `json:"firstname"`
-	LastName   string    `json:"lastname"`
-	AvatarURL  string    `json:"avatar_url"`
-	University string    `json:"university"`
-	IsOnline   bool      `json:"is_online"`
+    ID         uuid.UUID `json:"id"`
+    Username   string    `json:"username"`
+    FirstName  string    `json:"firstname"`
+    LastName   string    `json:"lastname"`
+    AvatarURL  string    `json:"avatar_url"`
+    University string    `json:"university"`
+    IsOnline   bool      `json:"is_online"`
 }
 
 func (f *FriendsInfoOut) toFriendsInfoOutForm(info models.FriendInfo, isOnline bool) FriendsInfoOut {
-	return FriendsInfoOut{
-		ID:         info.Id,
-		Username:   info.Username,
-		FirstName:  info.Firstname,
-		LastName:   info.Lastname,
-		AvatarURL:  info.AvatarURL,
-		University: info.University,
-		IsOnline:   isOnline,
-	}
+    return FriendsInfoOut{
+        ID:         info.Id,
+        Username:   info.Username,
+        FirstName:  info.Firstname,
+        LastName:   info.Lastname,
+        AvatarURL:  info.AvatarURL,
+        University: info.University,
+        IsOnline:   isOnline,
+    }
 }
 
 func (f *FriendsInfoOut) ToJson(friendsInfo []models.FriendInfo, friendsOnline []bool, friendsCount int) map[string]map[string]interface{} {
-	res := make(map[string]map[string]interface{})
-	res["body"] = make(map[string]interface{})
+    res := make(map[string]map[string]interface{})
+    res["payload"] = make(map[string]interface{})
 
-	var friendsInfoOut []FriendsInfoOut
-	for i, friendInfo := range friendsInfo {
-		friendsInfoOut = append(friendsInfoOut, f.toFriendsInfoOutForm(friendInfo, friendsOnline[i]))
-	}
+    var friendsInfoOut []FriendsInfoOut
+    for i, friendInfo := range friendsInfo {
+        friendsInfoOut = append(friendsInfoOut, f.toFriendsInfoOutForm(friendInfo, friendsOnline[i]))
+    }
 
-	res["body"]["friends"] = friendsInfoOut
-	res["body"]["total_count"] = friendsCount
+    res["payload"]["friends"] = friendsInfoOut
+    res["payload"]["total_count"] = friendsCount
 
-	return res
+    return res
 
 }
