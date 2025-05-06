8b924f82a2e9b7463b909470d4f2b02d3807a5eb
Nok1o <nikitamogilin39@gmail.com>
2025-05-06 15:08:21 +0300

    fix.

--- backend/feedback_service/internal/server.go
+++ backend/feedback_service/internal/server.go
@@ -1,66 +1,66 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "net"
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
 
-    _ "github.com/jackc/pgx/v5/stdlib"
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/credentials/insecure"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
-    micro_addr "quickflow/config/micro-addr"
-    postgres_config "quickflow/config/postgres"
-    grpc3 "quickflow/feedback_service/internal/delivery/grpc"
-    "quickflow/feedback_service/internal/delivery/interceptor"
-    postgres2 "quickflow/feedback_service/internal/repository/postgres"
-    "quickflow/feedback_service/internal/usecase"
-    userclient "quickflow/shared/client/user_service"
-    "quickflow/shared/interceptors"
-    proto "quickflow/shared/proto/feedback_service"
-    get_env "quickflow/utils/get-env"
+	micro_addr "quickflow/config/micro-addr"
+	postgres_config "quickflow/config/postgres"
+	grpc3 "quickflow/feedback_service/internal/delivery/grpc"
+	"quickflow/feedback_service/internal/delivery/interceptor"
+	postgres2 "quickflow/feedback_service/internal/repository/postgres"
+	"quickflow/feedback_service/internal/usecase"
+	userclient "quickflow/shared/client/user_service"
+	"quickflow/shared/interceptors"
+	proto "quickflow/shared/proto/feedback_service"
+	get_env "quickflow/utils/get-env"
 )
 
 func main() {
-    listener, err := net.Listen("tcp", fmt.Sprintf(":%d", micro_addr.DefaultFeedbackServicePort))
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
-    defer listener.Close()
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", micro_addr.DefaultFeedbackServicePort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	defer listener.Close()
 
-    grpcConnUserService, err := grpc.NewClient(
-        get_env.GetServiceAddr(micro_addr.DefaultFileServiceAddrEnv, micro_addr.DefaultFileServicePort),
-        grpc.WithTransportCredentials(insecure.NewCredentials()),
-        grpc.WithUnaryInterceptor(interceptors.RequestIDClientInterceptor()),
-        grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(micro_addr.MaxMessageSize)),
-    )
+	grpcConnUserService, err := grpc.NewClient(
+		get_env.GetServiceAddr(micro_addr.DefaultFileServiceAddrEnv, micro_addr.DefaultFileServicePort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(interceptors.RequestIDClientInterceptor()),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(micro_addr.MaxMessageSize)),
+	)
 
-    if err != nil {
-        log.Fatalf("failed to connect to file service: %v", err)
-    }
-    defer grpcConnUserService.Close()
+	if err != nil {
+		log.Fatalf("failed to connect to file service: %v", err)
+	}
+	defer grpcConnUserService.Close()
 
-    db, err := sql.Open("pgx", postgres_config.NewPostgresConfig().GetURL())
-    if err != nil {
-        log.Fatalf("failed to connect to postgres: %v", err)
-    }
+	db, err := sql.Open("pgx", postgres_config.NewPostgresConfig().GetURL())
+	if err != nil {
+		log.Fatalf("failed to connect to postgres: %v", err)
+	}
 
-    profileService := userclient.NewProfileClient(grpcConnUserService)
-    feedbackRepository := postgres2.NewFeedbackRepository(db)
-    feedbackUseCase := usecase.NewFeedBackUseCase(feedbackRepository)
+	profileService := userclient.NewProfileClient(grpcConnUserService)
+	feedbackRepository := postgres2.NewFeedbackRepository(db)
+	feedbackUseCase := usecase.NewFeedBackUseCase(feedbackRepository)
 
-    server := grpc.NewServer(
-        grpc.ChainUnaryInterceptor(
-            interceptors.RequestIDServerInterceptor(),
-            interceptor.ErrorInterceptor,
-        ),
-        grpc.MaxRecvMsgSize(micro_addr.MaxMessageSize),
-        grpc.MaxSendMsgSize(micro_addr.MaxMessageSize))
-    proto.RegisterFeedbackServiceServer(server, grpc3.NewFeedbackServiceServer(feedbackUseCase, profileService))
-    log.Printf("Server is listening on %s", listener.Addr().String())
+	server := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			interceptors.RequestIDServerInterceptor(),
+			interceptor.ErrorInterceptor,
+		),
+		grpc.MaxRecvMsgSize(micro_addr.MaxMessageSize),
+		grpc.MaxSendMsgSize(micro_addr.MaxMessageSize))
+	proto.RegisterFeedbackServiceServer(server, grpc3.NewFeedbackServiceServer(feedbackUseCase, profileService))
+	log.Printf("Server is listening on %s", listener.Addr().String())
 
-    if err = server.Serve(listener); err != nil {
-        log.Fatalf("failed to serve: %v", err)
-    }
+	if err = server.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
