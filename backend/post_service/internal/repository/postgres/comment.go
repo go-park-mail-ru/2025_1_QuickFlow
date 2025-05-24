cb648b471fb83d0e57913e873a1c3898b906fd1e
Nok1o <nikitamogilin39@gmail.com>
2025-05-24 18:06:15 +0300

    reorder comments

--- backend/post_service/internal/repository/postgres/comment.go
+++ backend/post_service/internal/repository/postgres/comment.go
@@ -26,7 +26,7 @@ const getCommentsForPostQuery = `
     select id, post_id, user_id, text, created_at, updated_at, like_count
     from comment
     where post_id = $1 and created_at < $2
-    order by created_at desc
+    order by created_at
     limit $3;
 `
 
